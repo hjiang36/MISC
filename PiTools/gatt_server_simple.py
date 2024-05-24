05cd5ea2c011cc7acaab77189309ee6a2f8ba2d2
Haomiao Jiang <hjiang36@gmail.com>
2024-05-23 23:41:37 -0700

    Create gatt_server_simple.py

---
+++ PiTools/gatt_server_simple.py
@@ -0,0 +1,44 @@
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+
+DBusGMainLoop(set_as_default=True)
+
+bus = dbus.SystemBus()
+manager = dbus.Interface(bus.get_object("org.bluez", "/"), "org.freedesktop.DBus.ObjectManager")
+
+# Custom service UUID
+SERVICE_UUID = "12345678-1234-5678-1234-567812345678"
+
+# Characteristic to hold the "hello" message
+CHARACTERISTIC_UUID = "9ABCDEF0-1234-5678-1234-567812345678"
+HELLO_MESSAGE = "hello"
+
+def register_service():
+    adapter_path = list(manager.GetManagedObjects().keys())[0]
+
+    # Register the custom service
+    service_props = {
+        "UUID": dbus.String(SERVICE_UUID),
+        "Primary": dbus.Boolean(True)
+    }
+    service_path = bus.get_object("org.bluez", adapter_path).AddService(service_props, dbus.Array([], signature=dbus.Signature('o')))
+
+    # Register the characteristic
+    char_props = {
+        "UUID": dbus.String(CHARACTERISTIC_UUID),
+        "Service": dbus.ObjectPath(service_path),
+        "Value": dbus.Array([dbus.Byte(ord(c)) for c in HELLO_MESSAGE], signature=dbus.Signature('y')),
+        "Flags": dbus.Array(["read"], signature=dbus.Signature('s'))
+    }
+    bus.get_object("org.bluez", service_path).AddCharacteristic(char_props, dbus.Array([], signature=dbus.Signature('o')))
+
+def start_advertising():
+    adapter = dbus.Interface(bus.get_object("org.bluez", adapter_path), "org.bluez.Adapter1")
+    adapter.StartDiscovery()
+    adapter.SetProperty("Discoverable", dbus.Boolean(True))
+
+register_service()
+start_advertising()
+
+GLib.MainLoop().run()
