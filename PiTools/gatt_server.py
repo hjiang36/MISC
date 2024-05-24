6d8b8b7da743f6262b8fc648e83800b64d6e8630
Haomiao Jiang <hjiang36@gmail.com>
2024-05-23 21:27:00 -0700

    Update gatt_server.py

--- PiTools/gatt_server.py
+++ PiTools/gatt_server.py
@@ -3,8 +3,60 @@ import dbus.mainloop.glib
 import dbus.service
 from gi.repository import GLib
 import subprocess
+import socket
 
 GATT_SERVICE_UUID = '12345678-1234-5678-1234-56789abcdef0'
+WIFI_CHARACTERISTIC_UUID = '87654321-4321-6789-4321-56789abcdef0'
+
+class WiFiCharacteristic(dbus.service.Object):
+    def __init__(self, bus, index, uuid, service):
+        self.path = f'/org/bluez/example/service{index}/char{index}'
+        self.bus = bus
+        self.uuid = uuid
+        self.service = service
+        self.flags = ['read']
+        dbus.service.Object.__init__(self, bus, self.path)
+        print(f"WiFiCharacteristic initialized: {self.path}")
+
+    def get_wifi_status(self):
+        try:
+            # Check if connected to WiFi
+            result = subprocess.run(['iwgetid'], capture_output=True, text=True)
+            if result.returncode != 0:
+                return "Not connected"
+            ssid = result.stdout.strip().split('"')[1]
+
+            # Get the IP address
+            ip_address = subprocess.run(['hostname', '-I'], capture_output=True, text=True).stdout.strip()
+            return f"Connected to {ssid}, IP: {ip_address}"
+        except Exception as e:
+            return f"Error: {str(e)}"
+
+    @dbus.service.method(dbus.PROPERTIES_IFACE, in_signature='ss', out_signature='v')
+    def Get(self, interface, prop):
+        if interface != 'org.bluez.GattCharacteristic1':
+            raise dbus.exceptions.DBusException("Invalid interface", name='org.freedesktop.DBus.Error.InvalidArgs')
+
+        if prop == 'UUID':
+            return self.uuid
+        elif prop == 'Value':
+            value = self.get_wifi_status()
+            return dbus.Array([dbus.Byte(c) for c in value.encode()], signature='y')
+        elif prop == 'Flags':
+            return self.flags
+        else:
+            raise dbus.exceptions.DBusException("Invalid property", name='org.freedesktop.DBus.Error.InvalidArgs')
+
+    @dbus.service.method(dbus.PROPERTIES_IFACE, in_signature='s', out_signature='a{sv}')
+    def GetAll(self, interface):
+        if interface != 'org.bluez.GattCharacteristic1':
+            raise dbus.exceptions.DBusException("Invalid interface", name='org.freedesktop.DBus.Error.InvalidArgs')
+
+        return {
+            'UUID': self.uuid,
+            'Value': dbus.Array([dbus.Byte(c) for c in self.get_wifi_status().encode()], signature='y'),
+            'Flags': self.flags,
+        }
 
 class Service(dbus.service.Object):
     def __init__(self, bus, index):
@@ -12,12 +64,16 @@ class Service(dbus.service.Object):
         self.bus = bus
         self.uuid = GATT_SERVICE_UUID
         self.primary = True
+        self.characteristics = []
         dbus.service.Object.__init__(self, bus, self.path)
+        self.add_characteristic(WiFiCharacteristic(bus, 0, WIFI_CHARACTERISTIC_UUID, self))
         print(f"Service initialized: {self.path}")
+
+    def add_characteristic(self, characteristic):
+        self.characteristics.append(characteristic)
 
     @dbus.service.method(dbus.PROPERTIES_IFACE, in_signature='ss', out_signature='v')
     def Get(self, interface, prop):
-        print(f"Get called: {interface}, {prop}")
         if interface != 'org.bluez.GattService1':
             raise dbus.exceptions.DBusException("Invalid interface", name='org.freedesktop.DBus.Error.InvalidArgs')
 
@@ -25,18 +81,20 @@ class Service(dbus.service.Object):
             return self.uuid
         elif prop == 'Primary':
             return self.primary
+        elif prop == 'Characteristics':
+            return dbus.Array([char.path for char in self.characteristics], signature='o')
         else:
             raise dbus.exceptions.DBusException("Invalid property", name='org.freedesktop.DBus.Error.InvalidArgs')
 
     @dbus.service.method(dbus.PROPERTIES_IFACE, in_signature='s', out_signature='a{sv}')
     def GetAll(self, interface):
-        print(f"GetAll called: {interface}")
         if interface != 'org.bluez.GattService1':
             raise dbus.exceptions.DBusException("Invalid interface", name='org.freedesktop.DBus.Error.InvalidArgs')
 
         return {
             'UUID': self.uuid,
             'Primary': self.primary,
+            'Characteristics': dbus.Array([char.path for char in self.characteristics], signature='o')
         }
 
 class Application(dbus.service.Object):
@@ -66,6 +124,11 @@ class Application(dbus.service.Object):
             response[service_path] = {
                 'org.bluez.GattService1': service.GetAll('org.bluez.GattService1')
             }
+            for characteristic in service.characteristics:
+                char_path = characteristic.path
+                response[char_path] = {
+                    'org.bluez.GattCharacteristic1': characteristic.GetAll('org.bluez.GattCharacteristic1')
+                }
         print(f"GetManagedObjects response: {response}")
         return response
 
@@ -80,7 +143,6 @@ def start_advertising():
     subprocess.run(['sudo', 'hciconfig', 'hci0', 'up'])
     subprocess.run(['sudo', 'hciconfig', 'hci0', 'leadv', '3'])
     subprocess.run(['sudo', 'hciconfig', 'hci0', 'noscan'])
-    # Start advertising using bluetoothctl
     subprocess.run(['sudo', 'bluetoothctl', 'advertise', 'on'])
     print("Advertising started")
 
