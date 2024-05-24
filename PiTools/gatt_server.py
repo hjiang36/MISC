3631d5ac34137abbd3d7dec76f9d8c92bdd7fad6
Haomiao Jiang <hjiang36@gmail.com>
2024-05-23 23:56:14 -0700

    Update gatt_server.py

--- PiTools/gatt_server.py
+++ PiTools/gatt_server.py
@@ -61,6 +61,7 @@ class Service(dbus.service.Object):
         self.uuid = GATT_SERVICE_UUID
         self.primary = True
         self.characteristics = []
+        self.type = "gatt"
         dbus.service.Object.__init__(self, bus, self.path)
         self.add_characteristic(WiFiCharacteristic(bus, 0, WIFI_CHARACTERISTIC_UUID, self))
         print(f"Service initialized: {self.path}")
@@ -112,20 +113,13 @@ class Application(dbus.service.Object):
     def GetAll(self, interface):
         return {}
 
-    @dbus.service.method('org.freedesktop.DBus.ObjectManager', in_signature='', out_signature='a{oa{sa{sv}}}')
+    @dbus.service.method('org.freedesktop.DBus.ObjectManager', out_signature='a{oa{sa{sv}}}')
     def GetManagedObjects(self):
         response = {}
         for service in self.services:
-            service_path = service.path
-            response[service_path] = {
-                'org.bluez.GattService1': service.GetAll('org.bluez.GattService1')
-            }
+            response[service.path] = service.get_properties()
             for characteristic in service.characteristics:
-                char_path = characteristic.path
-                response[char_path] = {
-                    'org.bluez.GattCharacteristic1': characteristic.GetAll('org.bluez.GattCharacteristic1')
-                }
-        print(f"GetManagedObjects response: {response}")
+                response[characteristic.path] = characteristic.get_properties()
         return response
 
 def register_app_cb():
